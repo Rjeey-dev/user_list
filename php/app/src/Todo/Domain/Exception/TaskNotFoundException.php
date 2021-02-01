6f46a3d05cef79edbd3c21aa44e30955d1ec11e8
Valera <valera.abashyn@gmail.com>
2021-02-01 18:11:36 +0300

    project creation

--- php/app/src/Todo/Domain/Exception/TaskNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Todo\Domain\Exception;
-
-class TaskNotFoundException extends \Exception
-{
-
-}
---
+++ php/app/src/User/Presentation/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\User\Presentation\Controller;
+
+use App\Kernel\Api\Controller\ApiController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends ApiController
+{
+
+    /**
+     * @Route("/lists", name="list", methods={"GET"})
+     */
+  public function lists (Request $request): Response
+  {
+      var_dump(22);
+
+
+
+  }
+}
