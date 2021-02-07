5c40eaca903a4427bd0a89eb72e2dc76155312fa
Valera <valera.abashyn@gmail.com>
2021-02-07 18:04:28 +0300

    create User and getting a list of users

--- php/app/src/User/Domain/Entity/User.php
+++ php/app/src/User/Domain/Entity/User.php
@@ -3,7 +3,74 @@ declare(strict_types=1);
 
 namespace App\User\Domain\Entity;
 
-class User
-{
+use App\User\Domain\Event\UserHasBeenCreatedEvent;
+use App\User\Domain\Event\UserHasBeenUpdateEvent;
+use App\User\Domain\Event\UserHasBeenDeletedEvent;
+use App\User\Domain\ValueObject\UserId;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use NinjaBuggs\ServiceBus\Event\EventRecordableInterface;
+use NinjaBuggs\ServiceBus\Event\EventRecordableTrait;
 
+/**
+ * @MongoDB\Document
+ */
+class User implements EventRecordableInterface
+{
+    use EventRecordableTrait;
+
+    /**
+     * @MongoDB\Id(strategy="NONE", type="todo:task_id")
+     */
+    private $id;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $username;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private $text;
+
+    /**
+     * @MongoDB\Field(type="date_immutable")
+     */
+    private $created;
+
+    public function __construct(UserId $id, string $username, string $text)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->text = $text;
+        $this->created = new \DateTimeImmutable();
+
+        $this->recordEvent(new UserHasBeenCreatedEvent(
+            $id->getId(),
+            $username,
+            $text
+
+        ));
+    }
+
+    public function update(string $text, string $username): void
+    {
+        $this->username = $username;
+        $this->text = $text;
+
+        $this->recordEvent(new UserHasBeenUpdateEvent(
+            $this->id->getId(),
+            $username,
+            $text
+        ));
+    }
+
+    public function delete(): void
+    {
+        $this->recordEvent(new UserHasBeenDeletedEvent(
+            $this->id->getId(),
+            $this->username,
+            $this->text
+        ));
+    }
 }
--- php/app/src/User/Presentation/Controller/UserController.php
+++ php/app/src/User/Presentation/Controller/UserController.php
@@ -4,38 +4,69 @@ declare(strict_types=1);
 namespace App\User\Presentation\Controller;
 
 use App\Kernel\Api\Controller\ApiController;
+use App\Kernel\Api\Response\ApiResponse;
+use App\User\Application\Command\CreateUserCommand;
+use App\User\Application\Query\FindUserByIdQuery;
+use App\User\Application\Query\FindUsersQuery;
+use App\User\Domain\DTO\UsersList;
+use App\User\Domain\Exception\ValidationException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
 
 class UserController extends ApiController
 {
     /**
      * @Route("/lists", name="list", methods={"GET"})
      */
-    public function lists(Request $request): Response
+    public function list(Request $request): Response
     {
-        $number = "FDSFDSF";
+        try {
+            /** @var UsersList $usersList */
+            $usersList = $this->queryBus->handle(new FindUsersQuery(
+                $request->query->get('offset') ? (string)$request->query->get('offset') : null,
+                $request->query->get('limit') ? (string)$request->query->get('limit') : null,
+                $request->query->get('order') ? (string)$request->query->get('order') : null
+            ));
 
-        return new Response($number);
+            return $this->buildSerializedListResponse(
+                $usersList->getTasks(),
+                $usersList->getTotalCount(),
+                ['users-list']
+            );
+        } catch (ValidationException $e) {
+            return $this->buildFailResponse(ApiResponse::ERROR_VALIDATION_FAILED);
+        } catch (\Throwable $e) {
+            return $this->buildFailResponse();
+        }
     }
 
     /**
-     * @Route("/create", name="create_user", methods={"POST"})
+     * @Route ("/create_user", name="create_user", methods={"POST"})
      */
     public function create(Request $request): Response
     {
+        try {
+            $data = json_decode($request->getContent(), true);
 
-    }
+            $command = new CreateUserCommand(
+                (string)$data['text'],
+                (string)$data['name']
+            );
 
-    /**
-     * @Route("/delete", name="user_delete", methods={"DELETE"})
-     */
-    public function delete()
-    {
+            $this->commandBus->handle($command);
 
-
-
+            return $this->buildSerializedResponse(
+                $this->queryBus->handle(new FindUserByIdQuery($command->getId()->getId()))
+                ['user-detail']
+            );
+        } catch (ValidationException $e) {
+            return $this->buildFailResponse(ApiResponse::ERROR_VALIDATION_FAILED);
+        } catch (\Throwable $e) {
+            return $this->buildFailResponse($e->getMessage());
+        }
     }
 }
 
